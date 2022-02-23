cc1c1fd17d4ba9d8d93b77644acdf39c88de9acf
Fabio Oliveira <fabio.dso.2000@gmail.com>
2022-02-23 12:53:21 +0100

    Added more functionality to StrHelper
    IsEmpty(value) - Returns true if the string is null, empty or white space.
    IsNotEmpty(value) - Returns true if the string contains any kind of character.
    LetterUpperCase(value, index) - Returns the string with the desired uppercase letter.
    LetterLowerCase(value, index) - Returns the string with the desired lowercase letter.

--- TsadriuUtilities/StrHelper.cs
+++ TsadriuUtilities/StrHelper.cs
@@ -49,5 +49,85 @@ namespace TsadriuUtilities
 
             return result;
         }
+
+        /// <summary>
+        /// Checks if the <paramref name="value"/> is null, <see cref="string.Empty"/> or a white space ("").
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <returns>Returns true if the <paramref name="value"/> is empty. Returns false if not.</returns>
+        public static bool IsEmpty(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return true;
+            }
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if <paramref name="value"/> contains any kind of character.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <returns>Returns true if the <paramref name="value"/> is not empty. Returns false if not.</returns>
+        public static bool IsNotEmpty(string value)
+        {
+            return !IsEmpty(value);
+        }
+
+        /// <summary>
+        /// Changes a lower-case letter to be upper-case. If <paramref name="index"/> is not passed, it will change the first letter of <paramref name="value"/>.
+        /// </summary>
+        /// <param name="value">The value to change.</param>
+        /// <param name="index">The index of the letter to be changed. If <paramref name="index"/> is not passed, it will change the first letter of <paramref name="value"/>.</param>
+        /// <returns>Returns the new string with the changed value. Returns the same <paramref name="value"/> if it was empty or <paramref name="index"/> was invalid.</returns>
+        private static string LetterUpperCase(string value, int index = 0)
+        {
+            if (IsNotEmpty(value))
+            {
+                if (index < value.Length)
+                {
+                    char[] charArray = value.ToCharArray();
+
+                    char charToModify = char.ToUpper(charArray[index]);
+
+                    charArray[index] = charToModify;
+
+                    return string.Concat(charArray);
+                }
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Changes a upper-case letter to be lower-case. If <paramref name="index"/> is not passed, it will change the first letter of <paramref name="value"/>.
+        /// </summary>
+        /// <param name="value">The value to change.</param>
+        /// <param name="index">The index of the letter to be changed. If <paramref name="index"/> is not passed, it will change the first letter of <paramref name="value"/>.</param>
+        /// <returns>Returns the new string with the changed value. Returns the same <paramref name="value"/> if it was empty or <paramref name="index"/> was invalid.</returns>
+        private static string LetterLowerCase(string value, int index = 0)
+        {
+            if (IsNotEmpty(value))
+            {
+                if (index < value.Length)
+                {
+                    char[] charArray = value.ToCharArray();
+
+                    char charToModify = char.ToLower(charArray[index]);
+
+                    charArray[index] = charToModify;
+
+                    return string.Concat(charArray);
+                }
+            }
+
+            return value;
+        }
     }
 }
