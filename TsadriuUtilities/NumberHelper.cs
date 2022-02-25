1cdddb026be350071698c177cacbe4aa37753498
Fabio Oliveira <fabio.dso.2000@gmail.com>
2022-02-25 17:12:08 +0100

    Added NumberHelper class
    Method ExponentialToDecimal() that converts an exponential number (46e-9) to a decimal.

---
+++ TsadriuUtilities/NumberHelper.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file Main.cs of solution TsadriuUtilities of developer Tsadriu>
+// Copyright 2022 (C) Tsadriu. All rights reserved.
+// </copyright>
+using System;
+
+namespace TsadriuUtilities
+{
+    /// <summary>
+    /// A class that helps on dealing with numbers.
+    /// </summary>
+    public class NumberHelper
+    {
+        /// <summary>
+        /// Converts an exponential number to a <see cref="decimal"/>. Example: 46e-9 returns 0.000000046.
+        /// </summary>
+        /// <param name="value">Number as a <see cref="string"/> (46e-9).</param>
+        /// <returns>Returns the parsed <paramref name="value"/> as a decimal. If the conversion fails, returns <see cref="decimal.Zero"/>.</returns>
+        public static decimal? ExponentialToDecimal(string value)
+        {
+            if (StrHelper.IsNotEmpty(value))
+            {
+                if (double.TryParse(value, out double valueAsDouble))
+                {
+                    return Convert.ToDecimal(valueAsDouble);
+                }
+            }
+
+            return decimal.Zero;
+        } 
+    }
+}
